97a4ca6eb4ff4b33fe443604c8ee8c34149f5499
DaidipyaMathur <daidipya_m@cs.iitr.ac.in>
2025-04-09 23:13:59 +0530

    Update urls.py

--- online_judge/online_judge/urls.py
+++ online_judge/online_judge/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for online_judge project.
+URL configuration for mindmaze project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.2/topics/http/urls/
@@ -14,10 +14,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('core.urls')),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),    
+    path('', include('authentication.urls')), # include authentication app urls
 ]
+
